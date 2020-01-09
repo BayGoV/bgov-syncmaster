42af50a4ae0067b0222365e61b185b67b0f8ccf5
meikai <meikai@seyes.org>
2020-01-09 13:29:59 +0100

    provide basic preference to bucket service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
+import { PreferencesService } from './preferences/preferences.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private preferenceService: PreferencesService) {}
 
   @Get()
   getHello(): string {
@@ -13,6 +16,9 @@ export class AppController {
 
   @Post('preferences')
   preferenceEvent(@Req() req: Request): string {
+    const jsonMessage = Buffer.from(req.body.message.data, 'base64').toString();
+    const preference = JSON.parse(jsonMessage);
+    this.preferenceService.pipeline.next(preference);
     return '';
   }
 
--- src/preferences/preferences.service.ts
+++ src/preferences/preferences.service.ts
@@ -1,30 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { Storage } from '@google-cloud/storage';
+import { PubSub } from '@google-cloud/pubsub';
 import { Duplex } from 'stream';
+import { from, Subject } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
 
+export class Preference {
+  id: string;
+  v: number;
+}
+
+// tslint:disable-next-line:max-classes-per-file
 @Injectable()
 export class PreferencesService {
   bucket;
+  pubsub;
+  pipeline: Subject<Preference>;
+  topicName = 'BgovBackendPreference';
+  bucketName = 'bgov-web-preferences';
 
   constructor() {
     const storage = new Storage();
-    this.bucket = storage.bucket('bgov-web-preferences');
+    this.pubsub = new PubSub();
+    this.bucket = storage.bucket(this.bucketName);
+    this.pipeline = new Subject<Preference>();
+    this.pipeline.pipe(
+      concatMap(preference => from(this.readCurrentAndVersion(preference))),
+      concatMap(preference => from(this.save(preference))),
+      concatMap(preference => from(this.saveResult(preference))),
+    );
   }
 
-  async readCurrent(id) {
-    let preference = null;
-    try {
-      const gcFile = this.bucket.file(id + '.pref');
-      const file = await gcFile.download();
-      preference = JSON.parse(file.toString());
-    } catch (e) {
-      // tslint:disable-next-line:no-console
-      console.error('Unable to read pref');
+  isWritable(oldPref, newPref) {
+    if (newPref.v !== oldPref.v + 1) {
+      throw new Error('Conflict detected. Aborting');
     }
-    return preference;
+    return newPref;
   }
 
-  save(preference, gcFile) {
+  async saveResult(preference) {
+    const data = JSON.stringify(preference);
+    const dataBuffer = Buffer.from(data);
+    return await this.pubsub.topic(this.topicName).publish(dataBuffer);
+  }
+
+  async readCurrentAndVersion(preference) {
+    const gcFile = this.bucket.file(preference.id + '.pref');
+    const file = await gcFile.download();
+    const oldPref = JSON.parse(file.toString());
+    return this.isWritable(oldPref, preference);
+  }
+
+  save(preference) {
+    const gcFile = this.bucket.file(preference.id + '.pref');
     return new Promise((resolve, reject) => {
       const stream = new Duplex();
       stream.push(preference);
@@ -44,7 +72,7 @@ export class PreferencesService {
           reject(error);
         })
         .on('finish', () => {
-          resolve(true);
+          resolve(preference);
         });
     });
   }
